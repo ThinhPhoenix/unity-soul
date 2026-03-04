c657c722525ad1906ce2f5b32f018b6df952c0dd
Gowwre <1stepawayfromfreedom@gmail.com>
2026-03-04 11:52:19 +0700

    add win/lost keyboard exit shortcuts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,18 +1,69 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private const int MainMenuSceneIndex = 0;
+    private const string WinSceneName = "Win";
+    private const string LostSceneName = "Lost";
+
+    private void Update()
+    {
+        if (!IsEndScene())
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Quit();
+            return;
+        }
+
+        bool wantsMainMenu =
+            Input.GetKeyDown(KeyCode.Return) ||
+            Input.GetKeyDown(KeyCode.KeypadEnter) ||
+            Input.GetKeyDown(KeyCode.Space);
+
+        if (wantsMainMenu)
+        {
+            LoadMainMenu();
+        }
+    }
+
+    private bool IsEndScene()
+    {
+        string sceneName = SceneManager.GetActiveScene().name;
+        return sceneName == WinSceneName || sceneName == LostSceneName;
+    }
+
     public void Play()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1 );
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         Time.timeScale = 1f;
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f;
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(MainMenuSceneIndex);
     }
 
     public void Quit()
     {
+        Time.timeScale = 1f;
+
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
+
         Debug.Log("Player Quit");
-    } 
+    }
 }
